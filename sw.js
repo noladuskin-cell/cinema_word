1d8bcd2dc76e00bffdd4ab17d2767d05f860a9ff
noladuskin-cell <noladuskin@gmail.com>
2026-05-17 19:32:32 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
 const CACHE_NAME = 'cinemaworld-v1';
+
+// Ресурсы, которые будут закэшированы для работы оффлайн
+// Используем относительные пути (./), так как sw.js лежит в корне
 const urlsToCache = [
   './',
   './index.html',
@@ -7,16 +10,44 @@ const urlsToCache = [
   './icons/icon-512x512.png'
 ];
 
+// Установка Service Worker и кэширование файлов
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
+// Перехват запросов и отдача кэша или загрузка из сети
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        // Если файл есть в кэше - возвращаем его
+        if (response) {
+          return response;
+        }
+        // Если нет - загружаем из интернета
+        return fetch(event.request);
+      })
+  );
+});
+
+// Удаление старого кэша при обновлении
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
