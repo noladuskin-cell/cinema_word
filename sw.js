f5ea7221d3dd6ff471e67c0eda7c20cc5fe4896e
noladuskin-cell <noladuskin@gmail.com>
2026-05-17 19:19:20 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'cinemaworld-v2';
-const ASSETS_TO_CACHE = [
+const CACHE_NAME = 'cinemaworld-v1';
+const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
@@ -7,60 +7,16 @@ const ASSETS_TO_CACHE = [
   './icons/icon-512x512.png'
 ];
 
-// Installation
-self.addEventListener('install', (event) => {
-  console.log('[SW] Installing...');
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('[SW] Caching assets');
-        return cache.addAll(ASSETS_TO_CACHE);
-      })
-      .then(() => self.skipWaiting())
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activation
-self.addEventListener('activate', (event) => {
-  console.log('[SW] Activating...');
-  event.waitUntil(
-    caches.keys()
-      .then((cacheNames) => {
-        return Promise.all(
-          cacheNames
-            .filter((name) => name !== CACHE_NAME)
-            .map((name) => caches.delete(name))
-        );
-      })
-      .then(() => self.clients.claim())
-  );
-});
-
-// Fetch
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-        return fetch(event.request)
-          .then((networkResponse) => {
-            if (networkResponse && networkResponse.status === 200) {
-              const responseClone = networkResponse.clone();
-              caches.open(CACHE_NAME)
-                .then((cache) => {
-                  cache.put(event.request, responseClone);
-                });
-            }
-            return networkResponse;
-          })
-          .catch(() => {
-            return new Response(
-              '<html><body style="font-family:sans-serif;background:linear-gradient(135deg,#0f0c29,#302b63);color:#fff;text-align:center;padding:50px;"><h1>📶 Офлайн-режим</h1><p>Проверьте подключение к интернету</p></body></html>',
-              { headers: { 'Content-Type': 'text/html; charset=utf-8' } }
-            );
-          });
-      })
+      .then(response => response || fetch(event.request))
   );
 });
